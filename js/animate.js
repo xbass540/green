296150fce9c473c8f359dd934ef3370d57fc5e51
Kostas <info@solomonsoft.gr>
2019-10-09 12:36:14 +0300

    only branch changes

--- js/animate.js
+++ js/animate.js
@@ -1,8 +1,8 @@
-//TweenMax.to('#logo1', 1, {x:800, y:800, ease: Power4.easeInOut, delay:0});
+TweenMax.to('#logo1', 1, {x:800, y:800, ease: Power4.easeInOut, delay:0});
 
 
-//TweenMax.to('#logo1', 1, {x:500, y:200, delay:2});
-//TweenMax.to('#logo1', 1, {x:200, y:700, delay:3});
+TweenMax.to('#logo1', 1, {x:500, y:200, delay:2});
+TweenMax.to('#logo1', 1, {x:200, y:700, delay:3});
 /*TweenMax.to('#logo2', 1, {y:800, delay:0.25});
 TweenMax.to('#logo3', 1, {y:800, delay:0.5});
 */
